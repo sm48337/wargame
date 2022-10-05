1275f5ea625578f5ff4632fcd334e2b25bb82813
Stjepan Marjanović <stjepan.marjanovic@reversinglabs.com>
2022-10-05 20:09:36 +0200

    Add Game model

--- wargame/models.py
+++ wargame/models.py
@@ -1,15 +1,16 @@
 from flask_login.mixins import UserMixin
+from sqlalchemy import Column, ForeignKey, String, Integer, Boolean, Text
+from sqlalchemy.orm import relationship
 from .db import db
 
 
 class User(db.Model, UserMixin):
     __tablename__ = 'user'
 
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String, unique=True)
-    password = db.Column(db.String)
-    authenticated = db.Column(db.Boolean, default=False)
-    active = db.Column(db.Boolean, default=True)
+    id = Column(Integer, primary_key=True)
+    username = Column(String, nullable=False, unique=True)
+    password = Column(String, nullable=False)
+    active = Column(Boolean, default=True)
 
     def is_active(self):
         return self.active
@@ -17,8 +18,34 @@ class User(db.Model, UserMixin):
     def get_id(self):
         return self.id
 
-    def is_authenticated(self):
-        return self.authenticated
-
     def is_anonymous(self):
         return False
+
+    def __repr__(self):
+        id, username = self.id, self.username
+        return f'<User {id=} {username=}>'
+
+    @property
+    def games(self):
+        return db.union(
+            Game.query.filter_by(first_player=self),
+            Game.query.filter_by(second_player=self),
+        ).all()
+
+    @property
+    def games_in_progress(self):
+        return self.games.filter_by(in_progress=True)
+
+
+class Game(db.Model):
+    __tablename__ = 'game'
+
+    id = Column(Integer, primary_key=True)
+    in_progress = Column(Boolean, default=True)
+    first_player_id = Column(Integer, ForeignKey('user.id'), nullable=False)
+    first_player = relationship('User', foreign_keys=[first_player_id])
+    second_player_id = Column(Integer, ForeignKey('user.id'), nullable=False)
+    second_player = relationship('User', foreign_keys=[second_player_id])
+    board_state = Column(Text)
+    victor_id = Column(Integer, ForeignKey('user.id'))
+    victor = relationship('User', foreign_keys=[victor_id])
