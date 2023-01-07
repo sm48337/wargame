731a544caa723f1ba76cd692f30022fb3218891e
Stjepan Marjanović <stjepan.marjanovic@reversinglabs.com>
2023-01-07 16:55:48 +0100

    Implement all actions

--- wargame/models.py
+++ wargame/models.py
@@ -1,8 +1,11 @@
+from random import randint
+
 from flask_login.mixins import UserMixin
 from sqlalchemy import Column, ForeignKey, String, Integer, Boolean, JSON, or_
 from sqlalchemy.ext.mutable import MutableDict, MutableList
 from sqlalchemy.orm import relationship
 from .db import db
+from .utils import attack_result_table, current_team, opposing_team, find_attack_targets, find_transfer_targets, teams, vitality_recovery_cost
 
 
 class User(db.Model, UserMixin):
@@ -49,24 +52,104 @@ class Game(db.Model):
         kwargs['history'] = [kwargs['board_state']]
         super().__init__(*args, **kwargs)
 
+    def get_current_entities(self):
+        turn = self.board_state['turn']
+        return self.board_state['teams'][current_team(turn)]['entities']
+
+    def get_entity(self, entity_id):
+        for team in teams:
+            for entity in self.board_state['teams'][team]['entities']:
+                if entity['id'] == entity_id:
+                    return entity
+
+    def get_all_connections(self, entity_id, entity_team):
+        entities = self.board_state['teams'][entity_team]['entities']
+        [entity] = filter(lambda e: e['id'] == entity_id, entities)
+        connected_entities = dict()
+        for connection in entities:
+            if connection['id'] in entity['connections']:
+                connected_entities[connection['id']] = connection
+        for candidate in entities:
+            if entity in candidate['connections']:
+                connected_entities[candidate['id']] = candidate
+        return connected_entities, entity
+
     def perform_checks(self, inputs):
         validation_errors = list()
 
-        if self.board_state['turn'] != int(inputs['turn']):
+        turn = self.board_state['turn']
+        if turn != int(inputs['turn']):
             validation_errors.append(('The turn had already ended!', 'error'))
 
-        if self.board_state['turn'] >= 23:
+        if turn >= 23:
             validation_errors.append(('The game is finished!', 'error'))
 
         return validation_errors
 
-    def process_inputs(game, inputs):
+    def _do_revitalize(self, entity, inputs):
+        vitality_recovered = int(inputs.get(entity['id'] + '__revitalize'))
+        recovery_cost = vitality_recovery_cost[vitality_recovered]
+        entity['vitality'] += vitality_recovered
+        entity['resource'] -= recovery_cost
+        self.message_log.append(f"{entity['name']} spent {recovery_cost} resources to gain {vitality_recovered} vitality.")
+
+    def _do_damage(self, target_id, amount, target_team):
+        connections, target = self.get_all_connections(target_id, target_team)
+        target['vitality'] -= amount
+        for connection in connections:
+            connection['vitality'] -= amount // 2
+        self.message_log.append(f"{target['name']} was dealt {amount} damage. Connected entities got {amount // 2} damage.")
+
+    def _do_attribution(self):
         ...
+
+    def _do_attack(self, entity, inputs):
+        turn = self.board_state['turn']
+        for target_id, field in find_attack_targets(entity['id'], inputs):
+            attack_investment = int(inputs.get(field))
+            dice_roll = randint(1, 6)
+            attack_success = attack_result_table[attack_investment][dice_roll]
+            self.message_log.append(f"{entity['name']} spent {attack_investment} resources and rolled {dice_roll}.")
+
+            if attack_success > 0:
+                self._do_damage(target_id, attack_success, opposing_team(turn))
+            elif attack_success < 0:
+                self._do_damage(entity['id'], -attack_success, current_team(turn))
+                self._do_attribution()
+
+            entity['resource'] -= attack_investment
+
+    def _do_transfer(self, entity, inputs):
+        for target_id, field in find_transfer_targets(entity['id'], inputs):
+            transfer_amount = int(inputs.get(field) or 0)
+            target = self.get_entity(target_id)
+            target['resource'] += transfer_amount
+            entity['resource'] -= transfer_amount
+            if transfer_amount:
+                self.message_log.append(f"{entity['name']} sent {transfer_amount} resources to {target['name']}.")
+
+    def process_inputs(self, inputs):
+        for entity in self.get_current_entities():
+            if action := inputs.get(entity['id'] + '__action'):
+                match action:
+                    case '' | 'none':
+                        continue
+                    case 'revitalize':
+                        self._do_revitalize(entity, inputs)
+                    case 'attack':
+                        self._do_attack(entity, inputs)
+                    case 'transfer':
+                        self._do_transfer(entity, inputs)
+
+    def give_resources(self):
+        for entity in self.get_current_entities():
+            if entity['id'].endswith('_gov'):
+                entity['resource'] += 3
+                self.message_log.append(f"{entity['name']} gains 3 resources.")
 
     def progress_time(self):
         turn = self.board_state['turn']
-        current_team = 'red' if turn % 2 == 0 else 'blue'
-        self.message_log.append(f'End of turn {turn // 2} for the {current_team} team.')
+        self.message_log.append(f'End of turn {turn // 2 + 1} for the {current_team(turn).capitalize()} team.')
         self.board_state['turn'] += 1
 
     def determine_winner(self):
@@ -84,10 +167,11 @@ class Game(db.Model):
 
     def process_turn(self, inputs):
         self.process_inputs(inputs)
+        self.give_resources()
 
         self.progress_time()
 
-        if self.board_state['turn'] == 4:
+        if self.board_state['turn'] == 2:
             self.enable_attacks()
         elif self.board_state['turn'] == 23:
             self.determine_winner()
--- wargame/utils.py
+++ wargame/utils.py
@@ -1,5 +1,6 @@
 from functools import cache
 from json import load
+from re import match
 
 
 @cache
@@ -8,6 +9,28 @@ def get_initial_board_state():
         state = load(f)
         return state
 
+
+def find_attack_targets(attacker, form):
+    for field in form:
+        if m := match(attacker + r'-([^-]+)__attack', field):
+            yield m.groups()[0], field
+
+
+def find_transfer_targets(source, form):
+    for field in form:
+        if m := match(source + r'-([^-]+)__transfer', field):
+            yield m.groups()[0], field
+
+
+attack_result_table = (
+    (0, 0, 0, 0, 0, 0, 0),
+    (0, 0, 1, 1, 1, 1, 2),
+    (0, 0, 1, 1, 1, 2, 2),
+    (0, -1, 0, 1, 2, 2, 3),
+    (0, -1, 0, 1, 2, 3, 4),
+    (0, -2, -1, 2, 3, 3, 4),
+    (0, -2, -1, 0, 3, 5, 6),
+)
 
 months = (
     'January',
@@ -24,11 +47,34 @@ months = (
     'December',
 )
 
-teams = ('Red', 'Blue')
+teams = ('red', 'blue')
+
+vitality_recovery_cost = [0, 1, 2, 4, 5, 6, 7]
+
+
+def calculate_max_revitalization(available_resources):
+    vitality_max_resource_cost = [0, 1, 2, 2, 4, 5, 6]
+    if available_resources >= len(vitality_max_resource_cost):
+        return vitality_max_resource_cost[-1]
+    return vitality_max_resource_cost[available_resources]
+
+
+def current_team(turn):
+    return teams[turn % 2]
+
+
+def opposing_team(turn):
+    return teams[1 - turn % 2]
+
+
+def turn_to_month(turn):
+    return f"{months[turn // 2]} / {current_team(turn).capitalize()} team's turn"
 
 
 def helper_functions():
-    def turn_to_month(turn):
-        return f"{months[turn // 2]} / {teams[turn % 2]} team's turn"
-
-    return dict(turn_to_month=turn_to_month)
+    return dict(
+        turn_to_month=turn_to_month,
+        current_team=current_team,
+        vitality_recovery_cost=vitality_recovery_cost,
+        calculate_max_revitalization=calculate_max_revitalization,
+    )
